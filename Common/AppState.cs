70d96bde164c8bebbbf81b8c0df58c138c6e5b22
anddrzejb <andrzej@neelyc.com.cy>
2021-06-24 08:32:55 +0200

    Initial commit

---
+++ Common/AppState.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace Common
+{
+    public class AppState
+    {
+        private readonly IConfiguration _configuration;
+        public int ConfigCount { get; set; }
+        public string ManuallySetProperty { get; set; }
+
+        public AppState(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConfigCount = configuration.GetChildren().Count();
+        }
+    }
+}
